ba6c88953c2f4656be3db1719f14c42041e1e7e6
DESKTOP-19T11Q7\gogis <saba.chkhvirkia@original-body.com>
2026-03-10 16:53:33 +0400

    Revert "Merge pull request #8 from the-original-body/tob-twenty/saba"
    This reverts commit 9afa688af400a522b0b24f628df7e1d7e5aed4b1, reversing
    changes made to 7aec5186db179bd919e8f5dcf375cfe9e9b3b854.

--- packages/twenty-server/src/engine/workspace-manager/workspace-migration/universal-flat-entity/utils/delete-universal-flat-entity-from-universal-flat-entity-and-related-entity-maps-through-mutation-or-throw.util.ts
+++ packages/twenty-server/src/engine/workspace-manager/workspace-migration/universal-flat-entity/utils/delete-universal-flat-entity-from-universal-flat-entity-and-related-entity-maps-through-mutation-or-throw.util.ts
@@ -10,7 +10,7 @@ import {
 import { type MetadataRelatedFlatEntityMapsKeys } from 'src/engine/metadata-modules/flat-entity/types/metadata-related-flat-entity-maps-keys.type';
 import { type MetadataUniversalFlatEntityAndRelatedUniversalFlatEntityMaps } from 'src/engine/metadata-modules/flat-entity/types/metadata-related-types.type';
 import { type MetadataUniversalFlatEntity } from 'src/engine/metadata-modules/flat-entity/types/metadata-universal-flat-entity.type';
-import { findFlatEntityByUniversalIdentifierOrThrow } from 'src/engine/metadata-modules/flat-entity/utils/find-flat-entity-by-universal-identifier-or-throw.util';
+import { findFlatEntityByUniversalIdentifier } from 'src/engine/metadata-modules/flat-entity/utils/find-flat-entity-by-universal-identifier.util';
 import { getMetadataFlatEntityMapsKey } from 'src/engine/metadata-modules/flat-entity/utils/get-metadata-flat-entity-maps-key.util';
 import { type UniversalFlatEntityMaps } from 'src/engine/workspace-manager/workspace-migration/universal-flat-entity/types/universal-flat-entity-maps.type';
 import { deleteUniversalFlatEntityFromUniversalFlatEntityMapsThroughMutationOrThrow } from 'src/engine/workspace-manager/workspace-migration/universal-flat-entity/utils/delete-universal-flat-entity-from-universal-flat-entity-maps-through-mutation-or-throw.util';
@@ -35,7 +35,11 @@ export const deleteUniversalFlatEntityFromUniversalFlatEntityAndRelatedEntityMap
     deleteUniversalFlatEntityFromUniversalFlatEntityMapsThroughMutationOrThrow({
       universalIdentifierToDelete: universalFlatEntity.universalIdentifier,
       universalFlatEntityMapsToMutate:
-        universalFlatEntityAndRelatedMapsToMutate[flatEntityMapsKey],
+        universalFlatEntityAndRelatedMapsToMutate[
+          flatEntityMapsKey
+        ] as UniversalFlatEntityMaps<
+          MetadataUniversalFlatEntity<typeof metadataName>
+        >,
     });
 
     const manyToOneRelations = ALL_MANY_TO_ONE_METADATA_RELATIONS[metadataName];
@@ -96,11 +100,14 @@ export const deleteUniversalFlatEntityFromUniversalFlatEntityAndRelatedEntityMap
         continue;
       }
 
-      const relatedUniversalFlatEntity =
-        findFlatEntityByUniversalIdentifierOrThrow({
-          universalIdentifier: universalForeignKeyValue,
-          flatEntityMaps: relatedUniversalFlatEntityMaps,
-        });
+      const relatedUniversalFlatEntity = findFlatEntityByUniversalIdentifier({
+        universalIdentifier: universalForeignKeyValue,
+        flatEntityMaps: relatedUniversalFlatEntityMaps,
+      });
+
+      if (!isDefined(relatedUniversalFlatEntity)) {
+        continue;
+      }
 
       if (
         !Object.prototype.hasOwnProperty.call(
@@ -117,11 +124,12 @@ export const deleteUniversalFlatEntityFromUniversalFlatEntityAndRelatedEntityMap
       const updatedRelatedEntity = {
         ...relatedUniversalFlatEntity,
         [universalFlatEntityForeignKeyAggregator]: (
-          (
-            relatedUniversalFlatEntity as unknown as Record<string, string[]>
-          )[universalFlatEntityForeignKeyAggregator] ?? []
+          (relatedUniversalFlatEntity as unknown as Record<string, string[]>)[
+            universalFlatEntityForeignKeyAggregator
+          ] ?? []
         ).filter(
-          (id) => id !== universalFlatEntity.universalIdentifier,
+          (universalIdentifier) =>
+            universalIdentifier !== universalFlatEntity.universalIdentifier,
         ),
       };
 
